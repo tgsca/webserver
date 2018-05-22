ce0c4151ed0be1a7886e18b3facb5f8d348e0f9b
Andreas <andreas.sczepanski@testgilde.de>
2018-05-22 23:31:15 +0200

    first commit

---
+++ controllers/testStatusController.js
@@ -0,0 +1,28 @@
+// get all documents
+exports.get = function (req, res) {
+    res.jsonp({
+        "status": [
+            {
+                "testcycle": "CDM Smoke",
+                "testcases": "42",
+                "passed": "75%",
+                "failed": "12%",
+                "unexecuted": "13%"
+            },
+            {
+                "testcycle": "Unite Smoke",
+                "testcases": "88",
+                "passed": "82%",
+                "failed": "14%",
+                "unexecuted": "2%"
+            },
+            {
+                "testcycle": "Customer Portal Smoke",
+                "testcases": "20",
+                "passed": "92%",
+                "failed": "2%",
+                "unexecuted": "6%"
+            }
+        ]
+    });
+};
