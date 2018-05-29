fd886b977776031981109a7041c3a491ddd34100
Andreas <andreas.sczepanski@testgilde.de>
2018-05-29 23:16:41 +0200

    data/db added to .gitignore

--- controllers/testStatusController.js
+++ controllers/testStatusController.js
@@ -9,10 +9,6 @@ var TestStatus = mongoose.model('TestStatus');
  * GET all test status
  */
 exports.get = function(req, res, next) {
-/*     var content = fs.readFileSync('./currentStatus.json');
-    var teststatus = JSON.parse(content);
-    res.jsonp(teststatus);
- */
     TestStatus.find().exec(function (err, testStatus) {
         var testStatusList = {};
         var key = 'status';
